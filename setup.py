7411be38469646f06e9f290540a9389c0a0aa36f
Eduard Carrerars <ecarreras@gisce.net>
2024-05-13 17:49:06 +0200

    bump to v0.6.3

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ setup(
     author='GISCE',
     author_email='devel@gisce.net',
     url='https://github.com/gisce/python-ooui',
-    version='0.6.2',
+    version='0.6.3',
     license='MIT',
     long_description='''Open Object User Interface for GISCE-ERP''',
     provides=['ooui'],
