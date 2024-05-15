6f82c329e6c55ed8fc75ba52e7ff409d4e496bd9
Release <github-actions@github.com>
2024-05-15 08:19:05 +0000

    Bump to v0.9.3

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ setup(
     author='GISCE',
     author_email='devel@gisce.net',
     url='https://github.com/gisce/python-ooui',
-    version='0.9.2',
+    version='0.9.3',
     license='MIT',
     long_description='''Open Object User Interface for GISCE-ERP''',
     provides=['ooui'],
