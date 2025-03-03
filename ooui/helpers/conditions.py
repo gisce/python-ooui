5743b4c85face3918b43749a7101f92e35f4b581
Eduard Carrerars <ecarreras@gisce.net>
2025-03-03 09:55:20 +0100

    feat(conditions): get involved fields of a condition

--- ooui/helpers/conditions.py
+++ ooui/helpers/conditions.py
@@ -5,6 +5,18 @@ from datetime import datetime
 from simpleeval import EvalWithCompoundTypes, DEFAULT_OPERATORS, DEFAULT_NAMES
 
 
+class GetFieldsDict(dict):
+
+    def __init__(self, *args, **kwargs):
+        super(GetFieldsDict, self).__init__(*args, **kwargs)
+        self.fields = set()
+
+    def __getitem__(self, key):
+        if key not in self:
+            self.fields.add(key)
+        return dict.get(self, key, key)
+
+
 class ConditionParser(object):
     def __init__(self, condition):
         self.raw_condition = condition
@@ -13,6 +25,18 @@ class ConditionParser(object):
         self.operators = DEFAULT_OPERATORS.copy()
         self.operators[ast.BitAnd] = operator.and_
         self.values = {'current_date': datetime.now().strftime('%Y-%m-%d')}
+
+    @property
+    def involved_fields(self):
+        magic_dict = GetFieldsDict()
+        magic_dict.update(self.values)
+        magic_dict.update(DEFAULT_NAMES)
+        for key, condition in self.conditions:
+            s = EvalWithCompoundTypes(
+                names=magic_dict, functions=self.functions, operators=self.operators
+            )
+            s.eval(condition)
+        return magic_dict.fields
 
     def eval(self, values):
         if not self.conditions:
